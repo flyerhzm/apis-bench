67647aefffea00d52ca2d48bc311d2aff642e0c7
Richard Huang <flyerhzm@gmail.com>
2013-04-23 21:23:42 +0800

    pagination for leaderboards

--- leaderboard-rails/app/models/leaderboard.rb
+++ leaderboard-rails/app/models/leaderboard.rb
@@ -1,4 +1,10 @@
 class Leaderboard < ActiveRecord::Base
+  PER_PAGE = 10
   belongs_to :game
   attr_accessible :name
+
+  def self.page(page)
+    offset = (page - 1) * PER_PAGE
+    offset(offset).limit(PER_PAGE)
+  end
 end
