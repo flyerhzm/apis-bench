6e65c56e6d417a361498594f001a229ec5a21607
Richard Huang <flyerhzm@gmail.com>
2013-04-23 17:16:43 +0800

    create leaderboard-rails

---
+++ leaderboard-rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeaderboardRails::Application.config.secret_token = '68421370966f1e38b2ee46ae645c7688c4d0a5682128fce6beab736a7d55cadb47f029ca905e60dddb8ba9998d1e4b94eee7c14480dabdc57c353363eb8fb155'
