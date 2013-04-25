f4015a8bb0a9b091fbe16c9a28fa7034609bb7b3
Richard Huang <flyerhzm@gmail.com>
2013-04-25 23:26:06 +0800

    send QUIT signal to old master unicorn process

--- leaderboard-rails/config/unicorn.rb
+++ leaderboard-rails/config/unicorn.rb
@@ -3,7 +3,6 @@ shared_path = "/home/deploy/sites/apis-bench/shared"
 worker_processes 4
 working_directory "/home/deploy/sites/apis-bench/current/leaderboard-rails"
 listen "/tmp/leaderboard-rails.unicorn.sock", :backlog => 64
-listen 8080, :tcp_nopush => true
 timeout 30
 pid shared_path + "/pids/leaderboard-rails.unicorn.pid"
 stderr_path shared_path + "/log/leaderboard-rails.unicorn.stderr.log"
@@ -14,8 +13,14 @@ GC.respond_to?(:copy_on_write_friendly=) and
 #check_client_connection false
 
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{shared_path}/pids/unicorn.pid.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      puts "Send 'QUIT' signal to unicorn error!"
+    end
+  end
 end
 
 after_fork do |server, worker|
