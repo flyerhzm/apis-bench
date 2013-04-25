aee39cd78632b4f2316b8fe14efc9c0412e1a597
Richard Huang <flyerhzm@gmail.com>
2013-04-25 15:32:13 +0800

    update unicorn god config

--- leaderboard-rails/config/unicorn.rb
+++ leaderboard-rails/config/unicorn.rb
@@ -1,17 +1,17 @@
-app_path = "/home/deploy/sites/apis-bench/current/leaderboard-rails"
+shared_path = "/home/deploy/sites/apis-bench/shared"
 
 worker_processes 4
-working_directory app_path
+working_directory "/home/deploy/sites/apis-bench/current/leaderboard-rails"
 listen "/tmp/leaderboard-rails.unicorn.sock", :backlog => 64
 listen 8080, :tcp_nopush => true
 timeout 30
-pid app_path + "/tmp/pids/leaderboard-rails.unicorn.pid"
-stderr_path app_path + "/log/leaderboard-rails.unicorn.stderr.log"
-stdout_path app_path + "/log/leaderboard-rails.unicorn.stdout.log"
+pid shared_path + "/pids/leaderboard-rails.unicorn.pid"
+stderr_path shared_path + "/log/leaderboard-rails.unicorn.stderr.log"
+stdout_path shared_path + "/log/leaderboard-rails.unicorn.stdout.log"
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) and
   GC.copy_on_write_friendly = true
-check_client_connection false
+#check_client_connection false
 
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and
