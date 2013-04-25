c61e14150a7cf67105ce858a76f7c11075fa232c
Richard Huang <flyerhzm@gmail.com>
2013-04-25 15:34:40 +0800

    add god chef

--- chef/site-cookbooks/main/recipes/default.rb
+++ chef/site-cookbooks/main/recipes/default.rb
@@ -4,3 +4,7 @@ user_account "deploy" do
   ssh_keys ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2qM48j/fVhoJlXnQ7i4XNj8wE8zpRJMJMT1PllPqCGW/IMdnQfoIw43fHtOCSyoEGmJ9IWtbxQK2/+XkcycIILw1EAfBYw5/DKi9EwvlFmyIe3sj2R9TdJ31Dv7wTlEOkYfY8dSUjSJc8aai2KmmWuvHudL4qhT7yxN4j2pFeoo6ACtnwxgYcrtG2jBeZtSqdW7mWBXZh0SJglo1lyMtZ2gmkqj8tutynZix6pxdAoP8gcQEWOjINxNJe+T+zbxsjkFZg0SrYK/ismO/ZjJvPYmbDMudxQhRyPx+KB4K1yEwn5xgzcnO6su8alXOIxCER7Bh7Bl51td9RaCx3dXdp huangrichard@Huangs-MacBook-Air.local"]
   supports manage_home: true
 end
+
+god_monitor "leaderboard-rails" do
+  config "leaderboard-rails.god.erb"
+end
