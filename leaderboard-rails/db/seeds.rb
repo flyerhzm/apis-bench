0dcf8e47aed2ebb80839016e36e4843476a8a0ad
Richard Huang <flyerhzm@gmail.com>
2013-05-03 23:00:39 +0800

    seed games data

--- leaderboard-rails/db/seeds.rb
+++ leaderboard-rails/db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+Game.delete_all
+
+10.times do |i|
+  Game.create name: Faker::Name.name
+end
