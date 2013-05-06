c2340e4c3e4dbb508dd0e692ccdb2da52e9c9368
Richard Huang <flyerhzm@gmail.com>
2013-05-06 16:31:41 +0800

    read database config in unicorn.rb

--- leaderboard-grape/config/unicorn.rb
+++ leaderboard-grape/config/unicorn.rb
@@ -24,6 +24,9 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  if defined?(ActiveRecord::Base)
+    env = ENV['RACK_ENV'] || "development"
+    config = YAML::load(File.open('config/database.yml'))[env]
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
