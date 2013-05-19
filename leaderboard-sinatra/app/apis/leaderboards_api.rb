b19bc93188184bbd0d24fb435b4e2ad569fb7c30
Richard Huang <flyerhzm@gmail.com>
2013-05-19 23:13:08 +0800

    update sinatra and use sinatra-contrib

--- leaderboard-sinatra/app/apis/leaderboards_api.rb
+++ leaderboard-sinatra/app/apis/leaderboards_api.rb
@@ -1,27 +1,29 @@
 class LeaderboardsAPI < BaseAPI
+  helpers Sinatra::JSON
+
   get '/games/:game_id/leaderboards.json' do
     @game = Game.find(params[:game_id])
     page = params[:page] ? params[:page].to_i : 1
     @leaderboards = @game.leaderboards.page(page)
-    halt MultiJson.encode(@leaderboards)
+    json @leaderboards
   end
 
   get '/games/:game_id/leaderboards/:id.json' do
     @game = Game.find(params[:game_id])
     @leaderboard = @game.leaderboards.find(params[:id])
-    halt MultiJson.encode(@leaderboard)
+    json @leaderboard
   end
 
   post '/games/:game_id/leaderboards.json' do
     @game = Game.find(params[:game_id])
     @leaderboard = @game.leaderboards.create(name: params[:name])
-    halt MultiJson.encode(@leaderboard)
+    json @leaderboard
   end
 
   put '/games/:game_id/leaderboards/:id.json' do
     @game = Game.find(params[:game_id])
     @leaderboard = @game.leaderboards.find(params[:id])
     @leaderboard.update_attributes(name: params[:name])
-    halt MultiJson.encode(@leaderboard)
+    json @leaderboard
   end
 end
--- leaderboard-sinatra/app/apis/users_api.rb
+++ leaderboard-sinatra/app/apis/users_api.rb
@@ -1,15 +1,17 @@
 class LeaderboardsAPI < BaseAPI
+  helpers Sinatra::JSON
+
   get '/leaderboards/:leaderboard_id/users.json' do
     @leaderboard = Leaderboard.find(params[:leaderboard_id])
     page = params[:page] ? params[:page].to_i : 1
     @scores = @leaderboard.scores.sort_by_value.page(page)
-    halt MultiJson.encode(@scores)
+    json @scores
   end
 
   get '/leaderboards/:leaderboard_id/users/:id.json' do
     @leaderboard = Leaderboard.find(params[:leaderboard_id])
     @user = User.find(params[:id])
     @score = @leaderboard.scores.where(user_id: @user.id).first
-    halt MultiJson.encode(score: @score.value, rank: @score.rank)
+    json score: @score.value, rank: @score.rank
   end
 end
