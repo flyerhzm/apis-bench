baa83f8e62b3946327fc203f2c064c940c0ebbf3
Richard Huang <flyerhzm@gmail.com>
2013-05-25 14:37:13 +0800

    add newrelic to grape-goliath

--- leaderboard-grape-goliath/config/application.rb
+++ leaderboard-grape-goliath/config/application.rb
@@ -2,5 +2,5 @@ env = ENV["RACK_ENV"] || "development"
 config = YAML::load(File.open('config/database.yml'))[env]
 ActiveRecord::Base.establish_connection(config)
 
-NewRelic::Agent.manual_start
+NewRelic::Agent.manual_start(env: env)
 DependencyDetection.detect!
